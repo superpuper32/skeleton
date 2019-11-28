550cfd475a1d042e63c05ec040888f987884d76e
superpuper32 <pochitalin_m@mail.ru>
2019-11-28 18:02:56 +0300

    Creating plugins with through2

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,18 @@
 'use strict';
 
 const gulp = require('gulp');
-const stylus = require('gulp-stylus');
-// const concat = require('gulp-concat');
-const debug = require('gulp-debug');
-const sourcemaps = require('gulp-sourcemaps');
-const gulpIf = require('gulp-if');
-const del = require('del');
-const newer = require('gulp-newer');
-const browserSync = require('browser-sync').create();
-const notify = require('gulp-notify');
-// const plumber = require('gulp-plumber');
-const multipipe = require('multipipe');
+const through2 = require('through2').obj;
 
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
-
-gulp.task('styles', function() {
-    return multipipe(
-        gulp.src('frontend/styles/main.styl'),
-        gulpIf(isDevelopment, sourcemaps.init()),
-        stylus(),
-        gulpIf(isDevelopment, sourcemaps.write()),
-        gulp.dest('public')
-    ).on('error', notify.onError());
-});
-
-gulp.task('clean', function() {
-    return del('public');
-});
 
 gulp.task('assets', function() {
-    return gulp.src('frontend/assets/**', {since: gulp.lastRun('assets')})
-        .pipe(newer('public'))
-        .pipe(debug({title: 'assets'}))
-        .pipe(gulp.dest('public'));
+
+    return gulp.src('frontend/assets/**/**.*')
+        .pipe(through2(function(file, enc, callback) {
+            let file2 = file.clone();
+            file2.path += '.bak';
+            this.push(file2);
+            callback(null, file);
+        }))
+        .pipe(gulp.dest('public'))
+
 });
-
-gulp.task('build', gulp.series(
-    'clean', 
-    gulp.parallel('styles', 'assets'))
-);
-
-gulp.task('watch', function() {
-    gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
-    gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
-});
-
-gulp.task('serve', function() {
-    browserSync.init({
-        server: 'public'
-    });
-    browserSync.watch('public/**/*.*').on('change', browserSync.reload)
-});
-
-gulp.task('dev',
-    gulp.series('build', gulp.parallel('watch', 'serve')));
