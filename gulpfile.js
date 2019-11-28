35b61cda42b9494e3efe1e81e7cdf9b5c3c49a83
superpuper32 <pochitalin_m@mail.ru>
2019-11-28 18:18:47 +0300

    Added manifest.json

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,31 @@
 
 const gulp = require('gulp');
 const through2 = require('through2').obj;
+const File = require('vinyl');
 
 
 gulp.task('assets', function() {
 
+    const mtimes = {};
+
     return gulp.src('frontend/assets/**/**.*')
-        .pipe(through2(function(file, enc, callback) {
-            let file2 = file.clone();
-            file2.path += '.bak';
-            this.push(file2);
-            callback(null, file);
-        }))
-        .pipe(gulp.dest('public'))
+        .pipe(through2(
+            
+            function(file, enc, callback) {
+                mtimes[file.relative] = file.stat.mtime;
+                callback(null, file);
+            },
+            function(callback) {
+                let manifest = new File({
+                    // cwd base path contents
+                    contents: new Buffer(JSON.stringify(mtimes)),
+                    base: process.cwd(),
+                    path: process.cwd() + '/manifest.json'
+                });
+                this.push(manifest);
+                callback();
+            }
+        ))
+        .pipe(gulp.dest('public'));
 
 });
