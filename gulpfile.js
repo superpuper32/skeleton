8928ffb1347c517872ee55c41604d3ad29a5d1c8
superpuper32 <pochitalin_m@mail.ru>
2019-11-28 17:42:19 +0300

    Cleaned gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,27 @@
 
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
-// const concat = require('gulp-concat'); .pipe(concat('all.css'))
-const debug = require('gulp-debug');  // .pipe(debug({title: 'src'})) .pipe(debug({title: 'stylus'})) .pipe(debug({title: 'concat'}))
+// const concat = require('gulp-concat');
+const debug = require('gulp-debug');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpIf = require('gulp-if');
 const del = require('del');
-const newer = require('gulp-newer'); // gulp-changed
+const newer = require('gulp-newer');
 const browserSync = require('browser-sync').create();
 const notify = require('gulp-notify');
 // const plumber = require('gulp-plumber');
 const multipipe = require('multipipe');
-// const combiner = require('stream-combiner2').obj;
 
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
 
 gulp.task('styles', function() {
-    // return gulp.src('frontend/styles/**/*.styl', {base: 'frontend'})
-    // console.log(require('stylus/lib/parser').cache);
-
     return multipipe(
         gulp.src('frontend/styles/main.styl'),
-        gulpIf(isDevelopment, sourcemaps.init()),  // file.sourceMap
-        stylus(), // async-done
+        gulpIf(isDevelopment, sourcemaps.init()),
+        stylus(),
         gulpIf(isDevelopment, sourcemaps.write()),
         gulp.dest('public')
     ).on('error', notify.onError());
-
 });
 
 gulp.task('clean', function() {
@@ -47,26 +42,16 @@ gulp.task('build', gulp.series(
 );
 
 gulp.task('watch', function() {
-
     gulp.watch('frontend/styles/**/*.*', gulp.series('styles'));
-
-    // chokidar
     gulp.watch('frontend/assets/**/*.*', gulp.series('assets'));
-
 });
 
 gulp.task('serve', function() {
     browserSync.init({
         server: 'public'
     });
-    
     browserSync.watch('public/**/*.*').on('change', browserSync.reload)
 });
 
 gulp.task('dev',
     gulp.series('build', gulp.parallel('watch', 'serve')));
-
-
-
-
-// process.on('exit');
